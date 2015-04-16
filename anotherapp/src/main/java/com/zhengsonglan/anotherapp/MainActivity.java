2f6d095ef568d45d7a527dedf31bf0bc1a06a001
zhengsl <zhengsl@tongxinyiliao.com>
2015-04-16 11:34:00 +0800

    another app启动service

--- anotherapp/src/main/java/com/zhengsonglan/anotherapp/MainActivity.java
+++ anotherapp/src/main/java/com/zhengsonglan/anotherapp/MainActivity.java
@@ -1,38 +1,61 @@
 package com.zhengsonglan.anotherapp;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.ComponentName;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+
+    Button bt_start, bt_stop;
+    String appPackage = "com.zhengsonglan.android_aidl";
+    String appService = "com.zhengsonglan.android_aidl.AppService";
+
+    Intent serviceIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+        initEvent();
+        initData();
+
+    }
+
+    private void initData() {
+        serviceIntent = new Intent();
+        serviceIntent.setComponent(new ComponentName(appPackage, appService));
+    }
+
+    private void initEvent() {
+        bt_start.setOnClickListener(this);
+        bt_stop.setOnClickListener(this);
+    }
+
+    private void initView() {
+        bt_start = (Button) findViewById(R.id.main_bt_start);
+        bt_stop = (Button) findViewById(R.id.main_bt_stop);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.main_bt_start:
+                startService(serviceIntent);
+                Toast.makeText(this,"服务启动",Toast.LENGTH_SHORT).show();
+                break;
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+            case R.id.main_bt_stop:
+                stopService(serviceIntent);
+                Toast.makeText(this,"服务关闭",Toast.LENGTH_SHORT).show();
+                break;
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
         }
-
-        return super.onOptionsItemSelected(item);
     }
+
 }
